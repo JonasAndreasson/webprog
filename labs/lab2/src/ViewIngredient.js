0f16a426fdf4e96de719dcc6bbd7adf966a2ff8b
Jonas Andreasson <50884511+JonasAndreasson@users.noreply.github.com>
2022-02-10 15:41:46 +0100

    removed console.log

--- labs/lab2/src/ViewIngredient.js
+++ labs/lab2/src/ViewIngredient.js
@@ -2,8 +2,6 @@ function ViewIngredient(props){
     var name = window.location.href.split("/view-ingredient/")[1];
     let inventory = props.inventory;
     let ingredient = inventory[name];
-    console.log(name);
-    console.log(ingredient);
     return(
         <div className="container col-12">
             <h3>Vegan: {ingredient.vegan ? 'yes' : 'no'}</h3>
